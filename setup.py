12d5cac1df15fe0b2d31c6fc409b434275aedf39
Erin Dahlgren <erin@zima.bump>
2013-06-05 13:49:30 -0700

    include extra package data

--- setup.py
+++ setup.py
@@ -3,6 +3,21 @@ from setuptools import setup
 setup(name="robohash",
       version="0.0.2",
       packages=["robohash"],
+      package_data={'robohash': ['blue/*/*', 
+                                 'brown/*/*',
+                                 'green/*/*',
+                                 'grey/*/*',
+                                 'orange/*/*',
+                                 'pink/*/*',
+                                 'purple/*/*',
+                                 'red/*/*',
+                                 'white/*/*',
+                                 'yellow/*/*',
+                                 'set2/*/*',
+                                 'set3/*/*',
+                                 'bg1/*',
+                                 'bg2/*',
+                                 ]},
       entry_points={
         'console_scripts' :
         '''
